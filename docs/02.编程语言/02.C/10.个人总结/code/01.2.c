94cd02137864e77c7e4006ad8e5fb892bd8b9af9
sbcwcso <lijunjie199502@gmail.com>
2021-08-15 20:35:59 +0800

    update

--- docs/02.编程语言/02.C/10.个人总结/code/01.2.c
+++ docs/02.编程语言/02.C/10.个人总结/code/01.2.c
@@ -5,6 +5,9 @@
 
 /*
 #pragma region output
+9000042496
+9000042496
+True
 #pragma endregion output
 */
 
@@ -18,8 +21,8 @@ int main(void)
   int a = 1;
   float b = 9000043000;
   float c = 9000042000;
-  printf("%.20f\n", b);
-  printf("%.20f\n", c);
+  printf("%.f\n", b);
+  printf("%.f\n", c);
   if (b == c)
     printf("True\n");
 }
